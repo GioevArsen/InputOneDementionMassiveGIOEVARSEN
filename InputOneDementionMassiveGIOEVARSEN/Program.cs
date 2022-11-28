b2c9cdae3476a0c8bcecc657e43a818c7b000ec1
студент <stud@dc.nosu>
2022-11-28 11:07:22 +0300

    Добавьте файлы проекта.

---
+++ InputOneDementionMassiveGIOEVARSEN/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InputOneDementionMassiveGIOEVARSEN
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // константный массив
+            int[] mas = { 5, 6, 8, 2, 8, 9 };
+
+            foreach(var item in mas)
+            {
+                Console.WriteLine(item);
+            }
+        }
+    }
+}
